701525e3bc0fe15f7710f2ab7e2962cc73be6f55
Gergo <pancho.puskas@protonmail.com>
2022-12-04 12:37:16 +0100

    formatted day 4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -10,8 +10,12 @@ fn main() {
         .lines()
         .map(|line| {
             line.split(",")
-                .map(|sections| sections.split("-").map(|x| x.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>())
+                .map(|sections| {
+                    sections
+                        .split("-")
+                        .map(|x| x.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>()
+                })
                 .collect::<Vec<Vec<i32>>>()
         })
         .collect::<Vec<Vec<Vec<i32>>>>();
@@ -35,14 +39,19 @@ fn main() {
     let real_overlaps: i32 = input
         .iter()
         .filter_map(|sections| {
-            if ((sections[0][0] >= sections[1][0]) && (sections[0][0] <= sections[1][1])) || ((sections[0][1] >= sections[1][0]) && (sections[0][1] <= sections[1][1])) {
+            if ((sections[0][0] >= sections[1][0]) && (sections[0][0] <= sections[1][1]))
+                || ((sections[0][1] >= sections[1][0]) && (sections[0][1] <= sections[1][1]))
+            {
                 Some(1)
-            } else if ((sections[1][0] >= sections[0][0]) && (sections[1][0] <= sections[0][1])) || ((sections[1][1] >= sections[0][0]) && (sections[1][1] <= sections[0][1])) {
+            } else if ((sections[1][0] >= sections[0][0]) && (sections[1][0] <= sections[0][1]))
+                || ((sections[1][1] >= sections[0][0]) && (sections[1][1] <= sections[0][1]))
+            {
                 Some(1)
             } else {
                 None
             }
-        }).sum();
+        })
+        .sum();
 
     println!("{real_overlaps}");
 }
